6d4a59df6ec7e2a3dc74b455b32e1d5cada581fe
Sebastian Kremarik <skremarik680@alumno.unlam.edu.ar>
2024-06-21 20:31:00 -0300

    Implemented core functionalities: player and map rendering

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+mod map;
+mod player;
+mod prelude {
+    pub use crate::map::*;
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+}
+
+use player::Player;
+use prelude::*;
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Rusted Dungeon")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
---
+++ src/map.rs
@@ -0,0 +1,59 @@
+use std::{task::Context, usize};
+
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Map {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                // idx is for index
+                let idx = map_idx(x, y);
+
+                match self.tiles[idx] {
+                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
+                    TileType::Wall => ctx.set(x, y, GREEN, BLACK, to_cp437('#')),
+                }
+            }
+        }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    // TODO: implement this func in a future
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
+}
+
+/// Calculate the index of the vector that has the tiles.
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
